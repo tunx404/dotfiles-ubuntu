3e31e6c65c8b5f614055fafd5ee18a5fc918eec8
tunx404 <tunx404@gmail.com>
2023-03-07 16:31:17 +0700

    tunx404

--- .scripts/push_dotfiles.sh
+++ .scripts/push_dotfiles.sh
@@ -1,2 +1,2 @@
 /usr/bin/git --git-dir=$HOME/.dotfiles-ubuntu/ --work-tree=$HOME commit -m "tunx404"
-/usr/bin/git --git-dir=$HOME/.dotfiles-ubuntu/ --work-tree=$HOME push origin master
+/usr/bin/git --git-dir=$HOME/.dotfiles-ubuntu/ --work-tree=$HOME push origin main
